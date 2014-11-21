4c609599181ac3e1baa38ce55d919d8231f6f33d
Remiii <repository@remibarbe.fr>
2014-11-21 10:27:22 +0100

    Clean Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
             new remiii\GlobalBundle\remiiiGlobalBundle(),
             new remiii\CorporateBundle\remiiiCorporateBundle(),
             new remiii\AWSDownloadCheckBundle\remiiiAWSDownloadCheckBundle(),
